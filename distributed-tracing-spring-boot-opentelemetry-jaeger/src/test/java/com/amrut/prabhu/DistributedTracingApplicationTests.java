ef5eecd4fe78d152fa00f4d9bedc4ac907b5247e
amrutprabhu <amrutprabhu42@gmail.com>
2023-04-11 21:44:57 +0200

    Adding dummy test

---
+++ distributed-tracing-spring-boot-opentelemetry-jaeger/src/test/java/com/amrut/prabhu/DistributedTracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amrut.prabhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedTracingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
